68e7f5473cd267d1c6f7a7db707a1db7ac27af05
alcalyxz <jposadasepulveda@gmail.com>
2020-11-15 09:49:21 -0500

    Subiendo Integracion con Firebas ey Analytics

---
+++ ExpisicionFireBase/app/src/main/java/co/edu/udea/compumovil/expisicionfirebase/AuthActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.expisicionfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
