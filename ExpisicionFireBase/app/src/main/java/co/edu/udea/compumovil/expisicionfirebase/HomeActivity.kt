0abb1e360459a7481339f46005ba8caa204526fd
alcalyxz <jposadasepulveda@gmail.com>
2020-11-17 05:49:42 -0500

    Finalizando

---
+++ ExpisicionFireBase/app/src/main/java/co/edu/udea/compumovil/expisicionfirebase/HomeActivity.kt
@@ -0,0 +1,34 @@
+package co.edu.udea.compumovil.expisicionfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
+
+enum class ProviderType{
+    BASIC
+}
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val bundle : Bundle?= intent.extras
+        val email : String? = bundle?.getString("email")
+        val provider : String? = bundle?.getString("provider")
+        //setup
+        setup(email ?: "", provider ?: "")
+    }
+
+    private fun setup(email: String, provider: String){
+        title = "Inicio"
+        emailTextView.text = email
+        providerTextView.text = provider
+
+        logOutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+    }
+}
